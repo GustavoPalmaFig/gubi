eca7f090ac024a1ff7674e0b2e5e991b00691874
Gustavo F <Gustavopalmafigueroa@outlook.com>
2025-04-15 22:49:09 -0300

    ✨ feat: implement authentication routing and create login/register components

---
+++ apps/gubi/src/app/features/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Route } from '@angular/router';
+
+export const AUTH_ROUTES: Route[] = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
+  }
+];
---
+++ apps/gubi/src/app/features/auth/pages/register/register.page.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register.page.html',
+  styleUrl: './register.page.scss',
+})
+export class RegisterPage {}
