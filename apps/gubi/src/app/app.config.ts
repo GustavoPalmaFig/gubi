52caf3f6afcb2d9cd25d4179f8d91ad46b9b33d9
Gustavo F <Gustavopalmafigueroa@outlook.com>
2025-04-15 22:47:22 -0300

    🚧 chore: refactor app configuration to include locale registration and import providers

--- apps/gubi/src/app/app.config.ts
+++ apps/gubi/src/app/app.config.ts
@@ -1,21 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 import Aura from '@primeng/themes/aura';
+import pt from '@angular/common/locales/pt';
 
 import { appRoutes } from './app.routes';
 
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      provideRouter(appRoutes),
-        provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
-    ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
 };
