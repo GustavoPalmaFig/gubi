48f419766d1d344b3f9e01116220b79ec0149f6c
Gustavo F <Gustavopalmafigueroa@outlook.com>
2025-05-13 19:48:26 -0300

    ✨ feat: Add user avatar component with dynamic background color and initials display

---
+++ apps/gubi/src/app/shared/components/user-avatar/user-avatar.component.ts
@@ -0,0 +1,37 @@
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
+import { Component, effect, input } from '@angular/core';
+import Utils from '@shared/utils/utils';
+
+@Component({
+  selector: 'app-user-avatar',
+  imports: [CommonModule, AvatarModule],
+  templateUrl: './user-avatar.component.html',
+  styleUrl: './user-avatar.component.scss'
+})
+export class UserAvatarComponent {
+  name = input.required<string>();
+  size = input<string>();
+
+  protected backgroundColor = '';
+
+  constructor() {
+    effect(() => {
+      this.backgroundColor = Utils.stringToColor(this.name());
+    });
+  }
+
+  protected get initials(): string {
+    const nameParts = this.name().split(' ');
+    return nameParts[0][0].toUpperCase();
+  }
+
+  protected get sizeClass(): string {
+    if (this.size() === 'large') {
+      return '!size-8';
+    } else if (this.size() === 'small') {
+      return '!size-4';
+    }
+    return '!size-6';
+  }
+}
